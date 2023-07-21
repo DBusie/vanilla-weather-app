5e6ddf917509886f5340ac8766be6fa7c543b96b
DBusie <132249012+DBusie@users.noreply.github.com>
2023-07-21 11:24:04 +0200

    Showing the current temperature

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#feel").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+}
 
+let city = "Bulawayo";
+let apiKey = "5e31eddd9b83339ff8cc05bb6263bcc3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
